e17eda870c3f371bcc813a00ebc69f67179eb9c3
ajmikallu <ajmiss9493@gmail.com>
2025-10-28 13:47:30 +0400

    Data fetch and search in customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,30 @@
 import { Metadata } from 'next';
- 
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { CreateCustomer } from '@/app/ui/customers/buttons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
-export default function Page() {
-  return <p>Customers Page</p>;
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const params = await searchParams;
+  const query = params?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  console.log('customers', customers);
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
