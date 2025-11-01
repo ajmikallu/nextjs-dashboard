8dcb9f4a5f19e557591ad994efdf57ebe2903de6
ajmikallu <ajmiss9493@gmail.com>
2025-11-01 16:06:30 +0400

    Add customer skeleton

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from 'next';
 import Table from '@/app/ui/customers/table';
-import { lusitana } from '@/app/ui/fonts';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
 import { fetchFilteredCustomers, fetchCustomersPages } from '@/app/lib/data';
 import Pagination from '@/app/ui/invoices/pagination';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Customers',
@@ -17,13 +18,12 @@ export default async function Page({
   const query = params?.query || '';
   const currentPage = Number(params?.page) || 1;
   const totalPages = await fetchCustomersPages(query);
-  console.log('totalPages', totalPages);
-
   const customers = await fetchFilteredCustomers(query, currentPage);
-  // console.log('customers', customers);
   return (
     <div className="w-full">
-      <Table customers={customers} />
+      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
