295beefdc45d262dcbf49708af2b2e6bfbb8f242
ajmikallu <ajmiss9493@gmail.com>
2025-11-06 00:08:43 +0400

    fix: suppress Chrome DevTools 404 warnings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+  return [
+    {
+      source: '/.well-known/:path*',      // ← If browser asks for this...
+      destination: '/api/not-found',      // ← Send them here instead
+    },
+  ];
+}
 };
 
 export default nextConfig;
