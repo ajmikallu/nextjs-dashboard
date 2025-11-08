4eec8608085c058fd1f16be3190d48fd74c3be7e
ajmikallu <ajmiss9493@gmail.com>
2025-11-09 01:44:53 +0400

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/ui/global.css";
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: {
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}
         <SpeedInsights />
+         <Analytics />
       </body>
     </html>
   );
